4d17e8ae4398916590ac331ee84d5545b81e1a10
satyamrai-automation <satyam636rai@gmail.com>
2026-08-18 17:25:44 +0530

    solved fmt - -recursive

--- eniverment/dev/main.tf
+++ eniverment/dev/main.tf
@@ -4,14 +4,14 @@ module "resource_group" {
 }
 
 module "virtual_network" {
-  depends_on = [ module.resource_group ]
-  source = "../../module/azurerm_virtual_network"
-  vnet   = var.vnet
+  depends_on = [module.resource_group]
+  source     = "../../module/azurerm_virtual_network"
+  vnet       = var.vnet
 }
 
 module "subnet" {
-  depends_on = [ module.resource_group , module.virtual_network ]
-  source = "../../module/azurerm_subnet"
-  snet   = var.snet
+  depends_on = [module.resource_group, module.virtual_network]
+  source     = "../../module/azurerm_subnet"
+  snet       = var.snet
 
 }
